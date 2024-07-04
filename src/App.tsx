b416ba1659ab4fe066a220d6dbfdb43b838530db
Dmitrii Kupchenko <kupchenko.work@gmail.com>
2024-07-04 16:56:09 +0200

    Add list filter with delayed search

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {useEffect, useState, useMemo} from "react";
+import faker from "faker";
 
-function App() {
+export async function getNamesApi() {
+  return Array.from(Array(500), () => {
+    return faker.name.findName();
+  });
+}
+
+function debounce(func: (val: string) => void, timeout = 1000) {
+  let timer: any;
+  return (val: string) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => func(val), timeout);
+  };
+}
+
+export function App() {
+  const [searchName, setSearchName] = useState<string>("");
+  const [allNames, setAllNames] = useState<Array<string>>([]);
+  const [filteredNames, setFilteredNames] = useState<Array<string>>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const namesResponse = await getNamesApi();
+      setAllNames(namesResponse);
+      setFilteredNames(namesResponse);
+    };
+    fetchData();
+  }, []);
+
+  const search = useMemo(
+    () =>
+      debounce((text: string) => {
+        console.log("Filtering", text);
+        setSearchName(text);
+      }),
+    []
+  );
+
+  useEffect(() => {
+    if (searchName) {
+      const names = allNames.filter((name: string) =>
+        name.toLowerCase().includes(searchName.toLowerCase())
+      );
+      setFilteredNames(names);
+    } else {
+      setFilteredNames(allNames);
+    }
+  }, [searchName, allNames]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <input
+          onChange={(event) => search(event.target.value)}
+          placeholder="Enter name"
+        />
+      </div>
+      {filteredNames.map((name, id) => {
+        return (
+          <>
+            <div>{name}</div>
+          </>
+        );
+      })}
+    </>
   );
 }
+
 
 export default App;
